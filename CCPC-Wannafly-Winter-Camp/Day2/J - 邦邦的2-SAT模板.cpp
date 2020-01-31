a337f3056b70ebf8a431904dc32fdb8b63a58d92
ChunYang Wang <42426838+HuoKuoluo@users.noreply.github.com>
2020-01-31 23:34:23 +0800

    清理重复文件

--- CCPC-Wannafly-Winter-Camp/Day2/J - 邦邦的2-SAT模板.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int n;
-int main(){
-    scanf("%d",&n); printf("%d\n",n);
-    printf("%d %d\n",-1,-n);
-    for(int i=1;i<n;++i)
-        printf("%d %d\n",-i,i+1);
-}
